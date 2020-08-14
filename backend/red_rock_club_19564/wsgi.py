078a1c0915f6940edea1bcb48f394e154420dc5f
Crowdbotics <team@crowdbotics.com>
2020-08-14 20:09:45 +0000

    Update models

--- backend/red_rock_club_19564/wsgi.py
+++ backend/red_rock_club_19564/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'red_rock_club_19564.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "red_rock_club_19564.settings")
 
 application = get_wsgi_application()
